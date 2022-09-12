33ece17c8b6510dde4b28ffe7c52aaac21296e15
Kyunghoon Moon <myrmidonxd@gmail.com>
2022-09-12 15:31:51 +0900

    revise baekjoon/#12865 - change 2D dp into 1D dp
    - 0-1 Knapsack can be solved by 1D dp, so changed the solution
    - https://chanhuiseok.github.io/posts/improve-6/

--- src/baekjoon/12865_ordinary_backpack.rs
+++ src/baekjoon/12865_ordinary_backpack.rs
@@ -1,7 +1,5 @@
 #[allow(unused_imports)]
 use std::io::{self, BufReader, BufRead, BufWriter, Write, Read};
-use std::cmp::Reverse;
-use std::collections::HashMap;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -11,39 +9,23 @@ struct TestCase {
     items: Vec<(usize, usize)>,
 }
 
-fn knapsack_dp(items: &Vec<(usize, usize)>, memo: &mut HashMap<(usize, usize), usize>, i: usize, k: usize) -> usize {
-    if memo.contains_key(&(i, k)) {
-        return memo[&(i, k)];
+fn knapsack_dp(items: &Vec<(usize, usize)>, k: usize) -> usize {
+    let mut dp = vec![0usize; k + 1];
+
+    for (w, v) in items {
+        for i in (*w..=k).rev() {
+            dp[i] = dp[i].max(dp[i-*w] + *v);
+        }
     }
 
-    if i >= items.len() || k == 0 {
-        return 0;
-    }
-
-    let (w, v) = items[i];
-    if w > k {
-        let max = knapsack_dp(items, memo, i+1, k);
-
-        memo.insert((i, k), max);
-        max
-    } else {
-        let mut max = v + knapsack_dp(items, memo, i+1, k-w);
-        max = max.max(knapsack_dp(items, memo, i+1, k));
-
-        memo.insert((i, k), max);
-        max
-    }
+    dp[k]
 }
 
 fn solve(tc: TestCase) -> usize {
-    let mut items = tc.items;
+    let items = tc.items;
     let k = tc.k;
 
-    items.sort_unstable_by_key(|e| Reverse(*e));
-
-    let mut memo: HashMap<(usize, usize), usize> = HashMap::new();
-
-    knapsack_dp(&items, &mut memo, 0, k)
+    knapsack_dp(&items, k)
 }
 
 #[allow(unused_variables)]
