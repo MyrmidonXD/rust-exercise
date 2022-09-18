b681be16c394530cbe64cbbcd98a8a59e168e30d
Kyunghoon Moon <myrmidonxd@gmail.com>
2022-09-18 19:44:50 +0900

    solve baekjoon/#11053

---
+++ src/baekjoon/11053_longest_increasing_subsequence.rs
@@ -0,0 +1,46 @@
+#[allow(unused_imports)]
+use std::io::{self, BufReader, BufRead, BufWriter, Write, Read};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct TestCase {
+    n: usize,
+    seq: Vec<usize>,
+}
+
+fn solve(tc: TestCase) -> usize {
+    let TestCase { n, seq } = tc;
+    let mut lis = vec![1; n];
+
+    for i in 1..n {
+        for j in 0..i {
+            if seq[j] < seq[i] {
+                lis[i] = lis[i].max(lis[j] + 1);
+            }
+        }
+    }
+
+    lis.into_iter().max().unwrap()
+}
+
+#[allow(unused_variables)]
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin = BufReader::new(stdin.lock());
+    let mut buffer = String::new();
+
+    let stdout = io::stdout();
+    let mut stdout = BufWriter::new(stdout.lock());
+
+    // Parsing
+    let _ = stdin.read_to_string(&mut buffer);
+    let mut inputs = buffer.split_whitespace().map(|s| s.parse::<usize>().unwrap());
+
+    let n = inputs.next().unwrap();
+    let seq = inputs.collect();
+
+    // Solve
+    let result = solve(TestCase { n, seq });
+    
+    let _ = writeln!(stdout, "{result}");
+}
